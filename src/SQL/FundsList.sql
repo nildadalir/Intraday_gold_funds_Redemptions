530c309cd2c9029b4a2e387d0d71f623866a311c
Nilda Dalir <nila.dalirnia@gmail.com>
2026-08-22 11:47:14 +0330

    Refactor SQL query in FundsList.sql to select additional fields including Instrument, AssetId, and Market. Update filtering criteria to include multiple market IDs, enhancing data retrieval for financial analysis.

--- src/SQL/FundsList.sql
+++ src/SQL/FundsList.sql
@@ -1,8 +1,12 @@
-select distinct TseId from DimInstrument 
-where 
-	IndustryId = 180 -- صندوق
-	and AssetClassId = 3 -- طلا 
-	and InstrumentId > 0 -- ETF
-	and Instrument not like '%اختیار%'
-	and Instrument not like '%شمش%'
-	and MarketId = 6 -- بازار معاملات اصلی
+SELECT
+    Instrument,
+    AssetId,
+    TseId,
+    Market
+FROM dbo.DimInstrument
+where AssetClassId = 3 -- طلا
+and IndustryId = 180 -- صندوق
+and marketid in (1,6)
+and InstrumentId > 0 -- ETF
+and Instrument not like '%اختیار%'
+and Instrument not like '%شمش%'
